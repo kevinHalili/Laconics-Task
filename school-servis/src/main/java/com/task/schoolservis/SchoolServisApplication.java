350847d30330ae38e17a4c1e56403f8b60aa7d81
Kevin Halili <kevin.halili@ids.al>
2023-06-30 09:40:00 +0200

    initail commit

---
+++ school-servis/src/main/java/com/task/schoolservis/SchoolServisApplication.java
@@ -0,0 +1,13 @@
+package com.task.schoolservis;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SchoolServisApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SchoolServisApplication.class, args);
+	}
+
+}
---
+++ school-servis/src/test/java/com/task/schoolservis/SchoolServisApplicationTests.java
@@ -0,0 +1,13 @@
+package com.task.schoolservis;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SchoolServisApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
